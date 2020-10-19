d1c47a5889ce9ffdbad0f1e6e69fae3ab0c58e8c
mishkamashka <anisimova.mariia.mm@gmail.com>
2020-10-19 13:18:08 +0300

    3 not finishe d

--- f2.py
+++ f2.py
@@ -1,16 +1,20 @@
 from bs4 import BeautifulSoup
 import re
-html = open("lab3/name2006.html").read()
+filename = "lab3/name2006.html"
+html = open(filename).read()
 soup = BeautifulSoup(html, features="html.parser")
-# table = soup.find("table")
-
-# print(table,"\n")
-output_rows = []
+  
+s = re.search(r"\D+(\d{4}).html", filename).group(1)
+all_names = []
+male_names = []
+female_names = []
 table_rows = re.findall(r"<tr align=\"right\"><td>\d+</td><td>\w+</td><td>\w+</td>", html)
 for row in table_rows:
     m = re.search(r"<tr align=\"right\"><td>(\d+)</td><td>(\w+)</td><td>(\w+)</td>",row)
-    output_rows.append(m.group(1))
-    output_rows.append(m.group(2))
+    male_names.append([m.group(2), int(m.group(1))])
+    female_names.append([m.group(3), int(m.group(1))])
+all_names = [s] + male_names + female_names
+all_names.sort(key = lambda x: x[0])
 
 
-print(output_rows)
+print(all_names[:10])
--- lab3/names.py
+++ lab3/names.py
@@ -1,13 +1,28 @@
+from bs4 import BeautifulSoup
+import re
+
 def extr_name(filename):
   """
   Вход: nameYYYY.html, Выход: список начинается с года, продолжается имя-ранг в алфавитном порядке.
   '2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' и т.д.
   """
-
-  male = []
-  felmale = []
+  html = open(filename).read()
+  soup = BeautifulSoup(html, features="html.parser")
   
-  return
+  s = re.search(r"\D+\(d{4}).html", filename).group(1)
+  all_names = []
+  male_names = []
+  female_names = []
+  table_rows = re.findall(r"<tr align=\"right\"><td>\d+</td><td>\w+</td><td>\w+</td>", html)
+  for row in table_rows:
+      m = re.search(r"<tr align=\"right\"><td>(\d+)</td><td>(\w+)</td><td>(\w+)</td>",row)
+      male_names.append([int(m.group(1)), m.group(2)])
+      female_names.append([int(m.group(1)), m.group(3)])
+  all_names = s + male_names + female_names
+  all_names.sort(key = lambda x: x[-1])
+  
+  names = (all_names, male_names, female_names)
+  return names
 
 
 def main():
@@ -15,8 +30,12 @@ def main():
   if not args:
     print 'use: [--file] file [file ...]'
     sys.exit(1)
-
-
+  elif:
+    for filename in args:
+      names = extr_name(filename)
+      print(filename, "\n", names[0])
+      print("TOP-10 male:\n", names[1][:10])
+      print("TOP-10 female:\n", names[2][:10])
   
   # для каждого переданного аргументом имени файла, вывести имена  extr_name
 
